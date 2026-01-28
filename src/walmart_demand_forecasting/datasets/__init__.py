ea62e332497ac731948974ca11dd8d95dd80f6b2
Zak-Las <lasmar.zakarya@gmail.com>
2026-01-28 15:53:54 -0500

    update to the init file to include the functions that live in m5_loaders.py

---
+++ src/walmart_demand_forecasting/datasets/__init__.py
@@ -0,0 +1,15 @@
+from walmart_demand_forecasting.datasets.m5_loaders import (  # noqa: F401
+    Paths,
+    load_ca_foods_lgbm,
+    load_ca_foods_nf,
+    load_global_lgbm,
+    load_global_nf,
+)
+
+__all__ = [
+    "Paths",
+    "load_ca_foods_lgbm",
+    "load_ca_foods_nf",
+    "load_global_lgbm",
+    "load_global_nf",
+]
